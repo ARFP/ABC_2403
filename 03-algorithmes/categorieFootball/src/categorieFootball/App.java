71fca980f0710699606d429348be57ea2e51d9c0
mdevoldere-119 <mdevoldere@users.noreply.github.com>
2024-04-30 14:53:59 +0200

    debut exo repro

---
+++ 03-algorithmes/categorieFootball/src/categorieFootball/App.java
@@ -0,0 +1,7 @@
+package categorieFootball;
+
+public class App {
+	public static void main(String args) {
+		
+	}
+}
---
+++ 03-algorithmes/jePredisLavenir/src/jePredisLavenir/App.java
@@ -0,0 +1,13 @@
+package jePredisLavenir;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Je prédis l'avenir");
+		System.out.println("Pas convaincu ? essayez donc !");
+		System.out.println("Veuillez entrer une heure (0:23)");
+		System.out.println("Veuillez entrer les minutes (0:59)");
+
+	}
+
+}
