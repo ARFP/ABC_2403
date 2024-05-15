957d66ad72038b63066fa6c435e45d4be3eb2ecf
mdevoldere-119 <mdevoldere@users.noreply.github.com>
2024-05-15 17:12:54 +0200

    increment

--- 03-algorithmes/demos/src/demos/App.java
+++ 03-algorithmes/demos/src/demos/App.java
@@ -1,14 +1,45 @@
 package demos;
 
+import java.util.Scanner;
 import classes.*;
 
 public class App {
 
 	public static void main(String[] args) {
-				
-		// DemoIfElse demo1 = new DemoIfElse();
-		DemoWhile demo2 = new DemoWhile();
-
+		
+		Scanner sc = new Scanner(System.in);
+		String saisie;
+		
+		do {
+			System.out.println("Sélectionner une démo : ");
+			saisie = sc.nextLine();
+			App.selectionnerDemo(saisie);
+			
+		} while(!saisie.equals("exit"));
+		
+		sc.close();
+	}
+	
+	public static void selectionnerDemo(String nomDemo)
+	{
+		switch(nomDemo) {
+			case "conditions":
+				new DemoIfElse();
+				break;
+			case "boucles":
+				new DemoWhile();
+				break;
+			case "exp":
+				new DemoExpressions();
+				break;
+			case "exit":
+				System.out.println("Programme terminé ! ");
+				System.exit(0);
+				break;
+			default: 
+				System.out.println("Saisie invalide !\n");
+				break;
+		}
 	}
 
 }
