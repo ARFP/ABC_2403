ada5c6360823553141965cc9303f961120dc038b
mdevoldere-119 <mdevoldere@users.noreply.github.com>
2024-05-31 10:01:52 +0200

    Update App.java

--- 03-algorithmes/insertionLegumes/src/insertionLegumes/App.java
+++ 03-algorithmes/insertionLegumes/src/insertionLegumes/App.java
@@ -8,7 +8,42 @@ public class App {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		String saisieUtilisateur;
 		
+		String[] legumesPrix = new String[1];
+		
+		String[] legumesPrix2;
+		
+		
+		do {
+			System.out.println("Saisissez un fruit ou un légume ");
+			
+			saisieUtilisateur = sc.nextLine();
+			
+			if(!saisieUtilisateur.equals("go")) {
+				
+				legumesPrix[legumesPrix.length - 1] = saisieUtilisateur;
+				
+				legumesPrix2 = new String[legumesPrix.length + 1];
+				
+				System.arraycopy(legumesPrix, 0, legumesPrix2, 0, legumesPrix.length);
+				
+				legumesPrix = legumesPrix2;
+			}
+		}
+		while(!saisieUtilisateur.equals("go"));
+		
+		
+		for(int i = 0; i < legumesPrix.length; i++){
+			System.out.println(legumesPrix[i]);
+		}
+		
+		/*for(int i = legumesPrix.length - 1; i >= 0; i--){ 
+			
+		}*/
+		
+		
+	
 		
 		sc.close();
 
